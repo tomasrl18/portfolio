924c8d00af664e4a9fac58a824b0d9a9befee15b
Tomás R <tomaslopezraigal@gmail.com>
2023-11-21 16:36:24 +0100

    Create displayText.js

---
+++ src/data/displayText.js
@@ -0,0 +1,8 @@
+function toggleSkills() {
+  var skillsList = document.querySelector(".skills-list");
+  if (skillsList.style.display === "none") {
+    skillsList.style.display = "block";
+  } else {
+    skillsList.style.display = "none";
+  }
+}
