67b8dd32547dd3369bcd4a2dbb185fb66088b334
Tomás R <tomaslopezraigal@gmail.com>
2023-11-21 17:16:29 +0100

    Update displayText.js

--- src/data/displayText.js
+++ src/data/displayText.js
@@ -6,3 +6,21 @@ function toggleSkills() {
     skillsList.style.display = "none";
   }
 }
+
+function toggleArcs() {
+  var arcs = document.querySelector(".arcs-list");
+  if (arcs.style.display === "none") {
+    arcs.style.display = "block";
+  } else {
+    arcs.style.display = "none";
+  }
+}
+
+function toggleSoftSkills() {
+  var softKills = document.querySelector(".soft-skills-list");
+  if (softKills.style.display === "none") {
+    softKills.style.display = "block";
+  } else {
+    softKills.style.display = "none";
+  }
+}
