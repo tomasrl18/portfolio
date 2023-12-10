eeb8e5c3edc659bf2820bbff9d7425e1bc067c66
Tomás R <tomaslopezraigal@gmail.com>
2023-12-10 20:16:16 +0100

    Fixed: The skills need two taps to display

--- src/data/displayText.js
+++ src/data/displayText.js
@@ -1,26 +1,22 @@
 function toggleSkills() {
   var skillsList = document.querySelector(".skills-list");
-  if (skillsList.style.display === "none") {
-    skillsList.style.display = "block";
-  } else {
-    skillsList.style.display = "none";
-  }
+  toggleVisibility(skillsList);
 }
 
 function toggleArcs() {
   var arcs = document.querySelector(".arcs-list");
-  if (arcs.style.display === "none") {
-    arcs.style.display = "block";
-  } else {
-    arcs.style.display = "none";
-  }
+  toggleVisibility(arcs);
 }
 
 function toggleSoftSkills() {
-  var softKills = document.querySelector(".soft-skills-list");
-  if (softKills.style.display === "none") {
-    softKills.style.display = "block";
+  var softSkills = document.querySelector(".soft-skills-list");
+  toggleVisibility(softSkills);
+}
+
+function toggleVisibility(element) {
+  if (window.getComputedStyle(element).display === "none") {
+    element.style.display = "block";
   } else {
-    softKills.style.display = "none";
+    element.style.display = "none";
   }
 }
